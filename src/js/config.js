6fbf5989c0f284d2beea4e3e94c1d61c49bfc133
jhoonhan <jhoon.han@outlook.com>
2022-02-06 12:12:09 -0500

    2/6 start

--- src/js/config.js
+++ src/js/config.js
@@ -7,11 +7,10 @@ export const NAMEMIN = 2;
 export const NAMEMAX = 30;
 export const NAMESHORTEN = 15;
 export const MAXSEARCHRESULT = 100;
-export const APIBASEURL = 'https://hanstudio-this-is-bullshit.herokuapp.com';
-export const APIARCHIVEURL = `${APIBASEURL}/archive`;
-export const APIAPIURL = `${APIBASEURL}/api/v1/artworks`;
-
-// export const APIBASEURL = 'http://127.0.0.1:3000';
+// export const APIBASEURL = 'https://hanstudio-this-is-bullshit.herokuapp.com';
 // export const APIARCHIVEURL = `${APIBASEURL}/archive`;
 // export const APIAPIURL = `${APIBASEURL}/api/v1/artworks`;
-/////////
+
+export const APIBASEURL = 'http://127.0.0.1:3000';
+export const APIARCHIVEURL = `${APIBASEURL}/archive`;
+export const APIAPIURL = `${APIBASEURL}/api/v1/artworks`;
